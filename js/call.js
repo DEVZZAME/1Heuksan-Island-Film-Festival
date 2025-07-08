5b9cb1a24cf9d6683648566a7345acb8d6aa6200
DEVZZAME <dev.zzame@gmail.com>
2025-07-08 22:49:44 +0900

    feat: 1st

---
+++ js/call.js
@@ -0,0 +1,12 @@
+    function openLink(){
+      window.open(
+        'https://docs.google.com/forms/d/1sbtf3GMcr-KonBNBudqorFfY7s-7BcBOe_PLoomOQoE/viewform?edit_requested=true',
+        '_blank'
+      );
+    }
+    
+
+    
+
+
+    
